6aac41c56273c6885db6f150a9bfd4f8050b3004
franius8 <skawinskifranciszek@gmail.com>
2022-09-28 21:42:30 +0200

    Create computer class

---
+++ hangman.rb
@@ -0,0 +1,13 @@
+class Computer
+  def draw_word
+    create_wordlist if @filtered_word_list.nil?
+    @filtered_word_list.sample
+  end
+
+  def create_wordlist
+    word_list = File.read('google-10000-english-no-swears.txt').split("\n")
+    @filtered_word_list = word_list.each_with_object([]) do |word, filtered_list|
+      filtered_list << word if word.length.between?(5, 12)
+    end
+  end
+end
