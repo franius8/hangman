b649237083c37420cd8aad72316ea1307ed1581d
franius8 <skawinskifranciszek@gmail.com>
2022-09-29 22:33:30 +0200

    Add comments for player class

--- lib/hangman.rb
+++ lib/hangman.rb
@@ -208,6 +208,7 @@ class Player
     @display = display
   end
 
+  # Methods used during initialization
   def play_again?
     @display.play_again_message
     return true if gets.chomp.downcase == 'y'
@@ -218,11 +219,13 @@ class Player
     return true if gets.chomp.downcase == 'y'
   end
 
+  # Methods used during the game
   def collect_guess
     @display.collect_guess_message
     gets.chomp.downcase
   end
 
+  # Methods used during saving/loading
   def collect_savefile_name
     @display.collect_savefile_name_message
     gets.chomp.downcase
